23290f4c437836c76894a4ba1048a7da11d2fb5b
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2023-04-03 14:37:23 +0000

    setup

--- src/HentaiVN/HentaiVN.test.ts
+++ src/HentaiVN/HentaiVN.test.ts
@@ -1,22 +1,24 @@
-import { Source } from 'paperback-extensions-common';
-import { HentaiVN } from './HentaiVN';
-import * as cheerio from 'cheerio';
-import { expect } from 'chai';
+// import { HomeSection, Source } from 'paperback-extensions-common';
+// import { HentaiVN } from './HentaiVN';
+// import * as cheerio from 'cheerio';
 
-describe("HentaiVN", function () {
-    let source: Source;
-    let hentaivn: HentaiVN;
+// describe("HentaiVN", function () {
+//     let source: Source;
+//     let hentaivn: HentaiVN;
 
-    before(async () => {
-        source = new HentaiVN(cheerio);
-        hentaivn = source as HentaiVN;
-    });
+//     source = new HentaiVN(cheerio);
+//     hentaivn = source as HentaiVN;
 
-    describe("getMangaDetails()", function () {
-        it('Get Chapter', async () => {
-            var data = await hentaivn.getMangaDetails('12302');
-            expect(data, 'Null').to.be.not.empty
-            console.log(data);
-        })
-    })
-})
+//     // describe("getHomePageSections()", function () {
+//     //     it('get HomePage Sections', async () => {
+            
+//     //     })
+//     // })
+//     // describe("getMangaDetails()", function () {
+//     //     it('Get Chapter', async () => {
+//     //         var data = await hentaivn.getMangaDetails('12302');
+//     //         expect(data, 'Null').to.be.not.empty
+//     //         console.log(data);
+//     //     })
+//     // })
+// })
--- src/HentaiVN/HentaiVN.ts
+++ src/HentaiVN/HentaiVN.ts
@@ -4,12 +4,48 @@ import {
     Manga,
     PagedResults,
     SearchRequest,
-    Source
+    Source,
+    Request,
+    Response,
+    HomeSection,
+    MangaTile,
+    SourceInfo,
+    ContentRating,
+    TagType
 } from "paperback-extensions-common";
+
+const DOMAIN = "https://hentaivn.tv";
+
+export const NettruyenInfo: SourceInfo = {
+    version: "1.0.0",
+    name: "HentaiVN",
+    icon: "icon.jpg",
+    author: "Hoang3409",
+    description: "Extension that pulls manga from HentaiVN.",
+    contentRating: ContentRating.ADULT,
+    websiteBaseURL: DOMAIN,
+    sourceTags: [{ text: 'Hentai', type: TagType.RED }]
+}
 
 export class HentaiVN extends Source {
     requestManager = createRequestManager({
-        requestsPerSecond: 0
+        requestsPerSecond: 5,
+        requestTimeout: 20000,
+        interceptor: {
+            interceptRequest: async (request: Request): Promise<Request> => {
+                request.headers = {
+                    ...(request.headers ?? {}),
+                    ...{
+                        'referer': DOMAIN
+                    }
+                }
+                return request
+            },
+
+            interceptResponse: async (response: Response): Promise<Response> => {
+                return response
+            }
+        }
     });
     override getMangaDetails(mangaId: string): Promise<Manga> {
         throw new Error("Method not implemented.");
@@ -22,5 +58,41 @@ export class HentaiVN extends Source {
     }
     override getSearchResults(query: SearchRequest, metadata: any): Promise<PagedResults> {
         throw new Error("Method not implemented.");
+    }
+    // 
+    override async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
+        let newAdded: HomeSection = createHomeSection({
+            id: 'new_added',
+            title: "Truyện Mới Cập Nhật",
+            view_more: true,
+        });
+
+        //Load empty sections
+        sectionCallback(newAdded);
+
+        //New Updates
+        let request = createRequestObject({
+            url: `${DOMAIN}/list-new2.php`,
+            method: "GET",
+        });
+
+        let data = await this.requestManager.schedule(request, 1);
+        let $ = this.cheerio.load(data.data);
+
+        newAdded.items = this.parseNewUpdatedSection($);
+        sectionCallback(newAdded);
+    }
+    parseNewUpdatedSection($: any): import("paperback-extensions-common").MangaTile[] {
+        const items: MangaTile[] = [];
+
+        for (let item of $('ul.page-random').toArray()) {
+            items.push(createMangaTile({
+                id: $('div.img-same > a', item).attr('href'),
+                title: createIconText({ text: '' }),
+                image: ""
+            }))
+        }
+
+        return items;
     }
 }
