ae7a774486f7196255175e80a81d3bd2fc5debcd
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2023-04-03 08:29:18 +0000

    Done test

--- src/Nettruyen/Nettruyen.test.ts
+++ src/Nettruyen/Nettruyen.test.ts
@@ -2,6 +2,7 @@ import { Nettruyen } from "./Nettruyen";
 // import assert from "assert";
 import {
     Source,
+    SearchRequest,
 } from "paperback-extensions-common";
 import * as cheerio from 'cheerio';
 import { expect } from "chai";
@@ -19,7 +20,22 @@ describe("Nettruyen", function () {
         it('Get Chapter', async () => {
             const data = await nettruyen.getChapters("60533");
             expect(data, 'Null').to.be.not.empty
-            console.log('Done');
+            // console.log(data);
+        })
+    });
+
+    describe("getSearchResults()", function () {
+        it('Get Search', async () => {
+            var meta = ''
+            var query: SearchRequest = {
+                title: 'One',
+                parameters: {
+                    includedTags: []
+                }
+            }
+            const data = await nettruyen.getSearchResults(query, meta);
+            expect(data, 'Null').to.be.not.empty
+            // console.log(data);
         })
     });
 });
