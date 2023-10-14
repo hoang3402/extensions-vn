5b523d006c75c9a5473b757b3ff1c2d067753ff7
hoang3402 <54090585+hoang3402@users.noreply.github.com>
2023-10-14 06:38:16 +0000

    Update gh-pages to output generated at b3df89a

--- LxManga/index.js
+++ LxManga/index.js
@@ -840,8 +840,8 @@ exports.Main = exports.getExportVersion = exports.TelegramApi = exports.Telegram
 const time_1 = require("./utils/time");
 exports.DOMAIN = 'https://hoang3409.link/api/';
 exports.TelegramEndpoint = 'https://api.telegram.org/';
-exports.TelegramApi = '6690512898:AAFvzwcfQ1axac2bDrTpRZDU4p3gFh_Gh1A';
-const BASE_VERSION = '1.5.0';
+exports.TelegramApi = '6458222681:AAEy9Q-qHskCvymzy3JYWxu-uM1jdC16cdk';
+const BASE_VERSION = '1.5.1';
 const getExportVersion = (EXTENSION_VERSION) => {
     return BASE_VERSION.split('.').map((x, index) => Number(x) + Number(EXTENSION_VERSION.split('.')[index])).join('.');
 };
